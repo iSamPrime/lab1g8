beaec51cc62b8d35244bc026baa63c3bed8fb4f0
linusgit22 <golinus07@gmail.com>
2026-09-25 19:19:39 +0200

    färdig funktion v1

--- Printing.py
+++ Printing.py
@@ -1,2 +1,6 @@
-#test
-""""""
+def printTopMost(frequencies,n): 
+    sorted_frequencies = sorted(frequencies.items(), key=lambda x: x[1],reverse=True) 
+    for i in range(n):
+        word, value = sorted_frequencies[i]
+        print(word.ljust(20),str(value).rjust(5))
+        
